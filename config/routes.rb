1df9825a448c4beb83cc52b266e5853b0bb0cee7
manap1031 <ikme.sabosan00@gmail.com>
2020-10-01 15:08:10 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :rooms
+  resources :rooms do
+    collection do
+      get 'search'
+    end
+  end
 end
